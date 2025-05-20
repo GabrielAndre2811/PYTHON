9d05b078fc0b313b83e1219b3034ec580f75b249
GABRIEL ANDRE GONCALVES <bielandregoncalves@gmail.com>
2025-05-20 11:59:29 -0300

    t

--- analisedadosnumeros.py
+++ analisedadosnumeros.py
@@ -4,29 +4,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-import lxml
+import lxml # Importa a biblioteca lxml para parsing de HTML
 
 url = "https://pt.wikipedia.org/wiki/Lista_de_unidades_federativas_do_Brasil_por_popula%C3%A7%C3%A3o"
 
 tabelas = pd.read_html(url)
 
 #print(len(tabelas))
-#print(tabelas[1].head())
+#print(tabelas)
 
 df = pd.read_html(url)[1]
 
 #print(df.head())
 #print(df.columns)
 
-df.columns.get_level_values(0)
-df.columns.get_level_values(1)
-
-print(df.columns.get_level_values(0).unique())
 print(df.columns.get_level_values(1).unique())
-print(df.columns.get_level_values(0).unique()[0])
-print(df.columns.get_level_values(1).unique()[0])
-print(df.columns.get_level_values(0).unique()[1])
-print(df.columns.get_level_values(1).unique()[1])
+#df.columns.get_level_values(1)
+
+#print(df.columns.get_level_values(0).unique())
+#print(df.columns.get_level_values(1).unique())
+#print(df.columns.get_level_values(0).unique()[0])
+#print(df.columns.get_level_values(1).unique()[0])
+#print(df.columns.get_level_values(0).unique()[1])
+#print(df.columns.get_level_values(1).unique()[1])
 
 
 
--- analisedadostexto.py
+++ analisedadostexto.py
@@ -1,15 +1,17 @@
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
+import requests # Importa a biblioteca requests para fazer requisições HTTP
+from bs4 import BeautifulSoup # Importa a biblioteca BeautifulSoup para fazer parsing de HTML
+import pandas as pd # Importa a biblioteca pandas para manipulação de dados
+import matplotlib.pyplot as plt # Importa a biblioteca matplotlib para visualização de dados
+import seaborn as sns # Importa a biblioteca seaborn para visualização de dados
+import numpy as np # Importa a biblioteca numpy para manipulação de arrays
 
-url = "https://g1.globo.com/"
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
 
-titulo = soup.find_all('div', class_='_evt')
+resposta = requests.get("https://g1.globo.com/")
+
+
+soup = BeautifulSoup(resposta.content, 'html.parser')
+
+titulo = soup.find_all('div', class_='_evt')[1]
 
 textos = [m.text.strip() for m in titulo]
 
